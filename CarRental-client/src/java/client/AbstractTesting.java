2a476048798ebfe049d019c6c69aeab730256e5d
Rik Bauwens <r0583265@peer.cs.kotnet.kuleuven.be>
2017-11-07 11:37:18 +0100

    fix error in abstracttesting

--- CarRental-client/src/java/client/AbstractTesting.java
+++ CarRental-client/src/java/client/AbstractTesting.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -139,7 +140,7 @@ public abstract class AbstractTesting {
 		List<String> result = new LinkedList<String>();
 		StringTokenizer stz = new StringTokenizer(regions, ":");
 		while (stz.hasMoreTokens()){
-			result.add(st.nextToken());
+			result.add(stz.nextToken());
 		}
 		return result;
 	}
